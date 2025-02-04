a8f4d286d91b971ff4cc8b54e0c348476953933a
Abisha-SM <abisha1720997@gmail.com>
2025-02-04 18:29:14 +0530

    -Added MainScene and update boundaries

---
+++ MergeFruit/Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private Button play;
+
+    private void Start()
+    {
+        play.onClick.AddListener(SwithScene);
+    }
+
+    private void SwithScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
